29f4197fe26a74e6e77b7d51647d3529241eeeb9
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-09-18 13:22:32 +0530

    added new programs

---
+++ 1. Basics/03_Strings.swift
@@ -0,0 +1,9 @@
+// String manipulations
+
+let movie = """
+Secret of the Omnitrix
+"""
+
+print(movie.count)
+print(movie.hasPrefix("Secret"))    // True
+print(movie.hasSuffix("omnitrix"))  // False : Case sensitive
---
+++ 1. Basics/04_IntegerOperations.swift
@@ -0,0 +1,12 @@
+// Operations on integers
+
+var number = 120
+print(number.isMultiple(of: 3))
+
+// Creating random numbers
+let id = Int.random(in: 1...1000)
+
+print(id)
+
+// floating point numbers
+let score = 3.0
