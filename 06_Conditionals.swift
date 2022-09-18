fbd7210f9d2112ad41ecc237c380ad9acb8ad745
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-09-18 13:32:41 +0530

    added new program

---
+++ 06_Conditionals.swift
@@ -0,0 +1,18 @@
+// Conditionals
+
+var age = 16
+
+if age >= 20 {
+    print("You are above 20")
+}
+else if age <= 20 {
+    print("You are below 20")
+}
+else {
+    print("You are 20")
+}
+
+
+// Ternary operator
+var isAbove20 = age >= 20 ? true : false
+print(isAbove20)
